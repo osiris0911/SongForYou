6d2751f90c0cfce6add89ef1486867a6dcc83ec9
osiris0911 <osiris0911@gmail.com>
2012-06-26 02:09:53 +0800

    init

---
+++ SongForYou/SongForYouTests/SongForYouTests.h
@@ -0,0 +1,13 @@
+//
+//  SongForYouTests.h
+//  SongForYouTests
+//
+//  Created by  on 12/6/26.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface SongForYouTests : SenTestCase
+
+@end
---
+++ SongForYou/SongForYouTests/SongForYouTests.m
@@ -0,0 +1,32 @@
+//
+//  SongForYouTests.m
+//  SongForYouTests
+//
+//  Created by  on 12/6/26.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import "SongForYouTests.h"
+
+@implementation SongForYouTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in SongForYouTests");
+}
+
+@end
